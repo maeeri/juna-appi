e3694e52f96b52af18cf6905b063c53e98b2f404
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-07-04 16:39:27 +0300

    uusi commit

--- JunaAppi/Program.cs
+++ JunaAppi/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-
-namespace JunaAppi
+namespace JunaProjekti
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool valikko = true;
+            while (valikko)
+            {
+                valikko = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            //Akin valikko
+            Console.WriteLine("Vaihtoehtosi:\n1) Mistä-Mihin\n2) Ajoissa\n3) Seuraava Pysäkki\n4) Vaihtoraide\n5) Junan Palvelut\n6) Poistu");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    return true;
+                case "2":
+                    return true;
+                case "3":
+                    return true;
+                case "4":
+                    return true;
+                case "5":
+                    return true;
+                case "6":
+                    return false;
+                default:
+                    return true;
+            }
         }
     }
 }
